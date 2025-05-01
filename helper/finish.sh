df81266c96fed4b79b56a9584248d26374089f7b
A.Farel.E <99515298+FarelE@users.noreply.github.com>
2025-05-01 18:18:41 +0700

    Update finish.sh

--- helper/finish.sh
+++ helper/finish.sh
@@ -8,26 +8,8 @@ function alertFinish() {
 
 }
 
-function alertNotification() {
-
-  IMAGE_PATH="${HOME}/.config/mytermux/alert/images"
-  IMAGE_FILE_NAME="finish.png"
-
-  termux-notification --sound -t "myTermux v${MYTERMUX_VERSION} has been installed" --image-path ${IMAGE_PATH}/${IMAGE_FILE_NAME}
-
-}
-
-function alertTorch() {
-
-  termux-toast -b "#A8D7FE" -c "#373E4D" -g middle "myTermux v${MYTERMUX_VERSION} has been installed"
-
-}
-
-
 function mainAlert() {
 
   alertFinish
-  alertNotification
-  alertTorch
 
 }
